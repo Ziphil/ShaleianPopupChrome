b85d89a5c065ddaf0a3d9db8e34df271055c7ea8
Ziphil <yfe23457@nifty.com>
2021-08-03 23:21:42 +0900

    ポップアップ部分にShadowDOMを用いるよう変更

--- source/script/popup.ts
+++ source/script/popup.ts
@@ -12,6 +12,9 @@ import {
 
 export class PopupExecutor extends Executor {
 
+  private popupElement!: HTMLElement;
+  private shadowElement!: ShadowRoot;
+
   protected execute(): void {
     this.appendPopup();
     this.addEventListeners();
@@ -19,14 +22,16 @@ export class PopupExecutor extends Executor {
 
   private appendPopup(): void {
     let popupElement = document.createElement("div");
+    let shadowElement = popupElement.attachShadow({mode: "closed"});
     popupElement.id = "shp-root";
     document.body.append(popupElement);
+    this.popupElement = popupElement;
+    this.shadowElement = shadowElement;
     console.log("[ShaleianPopup] An element for popups is appended");
   }
 
   private addEventListeners(): void {
     let linkElements = document.querySelectorAll<HTMLAnchorElement>(".sans a");
-    let popupElement = document.getElementById("shp-root")!;
     linkElements.forEach((linkElement) => {
       linkElement.addEventListener("mouseover", async () => {
         let name = linkElement.innerText;
@@ -34,8 +39,8 @@ export class PopupExecutor extends Executor {
         let word = (plainWord !== null && plainWord !== undefined) ? Parser.createSimple().parse(Word.fromPlain(plainWord)) : null;
         let html = (word !== null) ? PopupExecutor.createWordHtml(word) : null;
         if (html !== null) {
-          popupElement.innerHTML = html;
-          popupElement.style.display = "block";
+          this.shadowElement.innerHTML = html;
+          this.popupElement.style.display = "block";
           this.movePopupElement(linkElement);
         }
       });
@@ -43,7 +48,7 @@ export class PopupExecutor extends Executor {
   }
 
   private movePopupElement(targetElement: HTMLElement): void {
-    let popupElement = document.getElementById("shp-root")!;
+    let popupElement = this.popupElement;
     let rect = targetElement.getBoundingClientRect();
     let top = window.pageYOffset + rect.top - popupElement.offsetHeight - 10;
     let left = window.pageXOffset + rect.left - (popupElement.offsetWidth - targetElement.offsetWidth) / 2;
